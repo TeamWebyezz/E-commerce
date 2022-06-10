56c67b3f83f07faecdacb859b8e05e64c23b3315
TeamWebyezz <103387819+TeamWebyezz@users.noreply.github.com>
2022-06-10 16:37:14 +0530

    this is the best thing

---
+++ E commerce website /logicsheet/marqueeeffect,js/anotherMarqueeeffect.js
@@ -0,0 +1,21 @@
+// const cardpopUp = gsap.timeline({
+//   paused: "true"
+// })
+
+// cardpopUp.to(".card__pop__up",{
+//   duration: 0.6,
+//   y: -50,
+//   ease: Power4.easeOut
+
+// })
+
+// function cardPopUp(){
+//   cardpopUp.play()
+// }
+// gsap.from(".product__card",{
+//   y: 100,
+//   duration: 0.6,
+//   ease: Power4.easeOut,
+
+
+// })
